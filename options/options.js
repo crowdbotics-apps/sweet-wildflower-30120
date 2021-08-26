9bcc6c1b125abd59528236b34f37fbe7c8bf0d33
Crowdbotics <team@crowdbotics.com>
2021-08-26 09:48:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sweet_wildflower_30120Identifier",
+  url: "https://sweet_wildflower_30120Identifier.botics.co",
+  api: "https://sweet_wildflower_30120Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
